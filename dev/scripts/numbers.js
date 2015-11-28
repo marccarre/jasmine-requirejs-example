b3a9dee403de363708d752c5b22f6a1c96c07676
Marc CARRE <carre.marc@gmail.com>
2015-11-28 16:24:03 +0000

    Added implementation. Added more advanced test cases.

--- dev/scripts/numbers.js
+++ dev/scripts/numbers.js
@@ -2,6 +2,20 @@
 define(['events'], function(events) {
   'use strict';
   var self = {};
-
+  self.add = function add() {
+    var operands = Array.prototype.slice.call(arguments);
+    var total = 0;
+    operands.forEach(function(value) {
+      if (typeof value === 'string') {
+        value = parseInt(value, 10) || 0;
+      }
+      total += value;
+    })
+    events.publish('added', {
+      operands: operands,
+      result: total
+    })
+    return total;
+  }
   return self;
 });
--- test/spec/numbers.js
+++ test/spec/numbers.js
@@ -14,6 +14,21 @@ define(['numbers', 'events'], function(numbers, events) {
         expect(numbers.add(1, 2, 3)).toEqual(6);
       });
 
+      it('should try to parse string arguments as integers', function() {
+        expect(numbers.add(1, '2')).toEqual(3);
+      });
+
+      it('should ignore string arguments which are not parseable', function() {
+        expect(numbers.add(1, 'oops')).toEqual(1);
+      });
+
+      it('should publish an added event showing the operands passed to the method and the result', function() {
+        spyOn(events, 'publish');
+        numbers.add(1, 2);
+        expect(events.publish).toHaveBeenCalled();
+        expect(events.publish).toHaveBeenCalledWith('added', {operands: [1, 2], result: 3});
+      });
+
       afterEach(function() {
         // TODO
       });
